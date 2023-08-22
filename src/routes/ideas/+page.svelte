6d5a968bc9107b93223a5034691c6079e0f3fcd3
Lance J <lancear153@gmail.com>
2023-08-22 23:04:53 +0200

    fix first label color in select

--- src/routes/ideas/+page.svelte
+++ src/routes/ideas/+page.svelte
@@ -5,13 +5,13 @@
   
   let search = "";
   /** @type {number|null} */
-  let selectedState = null;
+  let selectedGrowth = null;
   /** @type {number|null} */
   let selectedColor = null;
   
   const allIdeasPromise = getAllIdeas();
   $: filteredIdeas = allIdeasPromise.then(ideas => ideas.filter((idea) => {
-    if (selectedState && idea.growth !== selectedState) return false;
+    if (selectedGrowth && idea.growth !== selectedGrowth) return false;
     if (selectedColor && idea.label !== selectedColor) return false;
     return idea.title.toLocaleLowerCase().includes(search.toLocaleLowerCase());
   }));
@@ -19,13 +19,13 @@
 
 <div class="m-4 mb-2">
   <input bind:value={search} placeholder="Search" type="search" class="w-full outline-none p-2 rounded-lg {search ? 'bg-slate-700' : 'bg-slate-600'} placeholder:text-slate-400" />
-  <select bind:value={selectedState} class="w-1/9 mt-2 p-1 text-sm rounded-md {selectedState ? 'bg-slate-700' : 'bg-slate-600'} text-slate-400">
-    <option value={null}>State</option>
+  <select bind:value={selectedGrowth} class="w-1/9 mt-2 p-1 text-sm rounded-md {selectedGrowth ? 'bg-slate-700' : 'bg-slate-600'} text-slate-400">
+    <option value={null}>Growth</option>
     {#each states as state}
       <option value={state.value}>{state.title}</option>
     {/each}
   </select>
-  <select bind:value={selectedColor} class="w-1/4 mt-2 p-1 text-sm rounded-md bg-{selectedColor ? `${colors[selectedColor]}-200 text-slate-600` : 'slate-600 text-slate-400'}">
+  <select bind:value={selectedColor} class="w-1/4 mt-2 p-1 text-sm rounded-md bg-{selectedColor !== null ? `${colors[selectedColor]}-200 text-slate-600` : 'slate-600 text-slate-400'}">
     <option value={null} class="bg-slate-600 text-slate-400">All Colors</option>
     {#each colors as color, colorIdx}
       <option value={colorIdx} class="bg-{color}-200">{color.charAt(0).toLocaleUpperCase()}{color.substring(1)}</option>
